323f5a9ead5ec8a686050b53ba24538c9b3ad100
Saul Saavedra <Sas8037@icloud.com>
2024-11-23 06:34:59 -0600

    Created file called Point.swift and added a structure called point

---
+++ AISketch/Models/Point.swift
@@ -0,0 +1,14 @@
+//
+//  Point.swift
+//  AISketch
+//
+//  Created by Saul Saavedra on 11/23/24.
+//
+
+import SwiftUI
+
+struct Point: Identifiable {
+    let id = UUID()
+    var x: CGFloat
+    var y: CGFloat
+}
