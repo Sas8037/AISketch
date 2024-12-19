a4b1a8886948658aa90f2f7506b516aeb7fadacd
Saul Saavedra <Sas8037@icloud.com>
2024-12-19 01:50:24 -0600

    Initial Commit

---
+++ AISketch/AISketchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISketchApp.swift
+//  AISketch
+//
+//  Created by Saul Saavedra on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AISketchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
